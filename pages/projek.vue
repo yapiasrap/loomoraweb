22112ab04371e5619bac12c30e45f9af4942098c
yapiasrap <147707830+yapiasrap@users.noreply.github.com>
2025-04-21 19:53:10 +0700

    update

--- pages/projek.vue
+++ pages/projek.vue
@@ -12,63 +12,114 @@ const banner = { judul: "PROYEK KAMI" };
 
 const images = [
   {
-    src: "https://firebasestorage.googleapis.com/v0/b/loomora-cdb63.firebasestorage.app/o/images%2Fproject%2F3.jpeg?alt=media&token=8fd4a626-9cf6-4eac-9119-e6bdc9f75dca",
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/c08d618c-1fa4-482e-37b8-2098e6680200/kotak300",
     text: "Ballroom Timor Leste",
     category: "Karpet Ballroom",
   },
-
   {
-    src: "https://firebasestorage.googleapis.com/v0/b/loomora-cdb63.firebasestorage.app/o/images%2Fproject%2F4.jpeg?alt=media&token=568d0b1f-5f2d-40cb-a4be-e4101f10bc83",
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/21c2cd2f-16c2-43c5-8b1a-6f4b4d6a5700/kotak300",
     text: "Ballroom Sucofindo - Jakarta",
     category: "Karpet Ballroom",
   },
-
   {
-    src: "https://firebasestorage.googleapis.com/v0/b/loomora-cdb63.firebasestorage.app/o/images%2Fproject%2F5.jpeg?alt=media&token=99093ffe-47a1-40e1-bd50-b715c223914d",
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/08dc9ff1-811a-4ee2-fd70-3ebea4ceea00/kotak300",
     text: "PX Pavillion (Lippo Mall Puri)",
     category: "Karpet Ballroom",
   },
-
   {
-    src: "https://firebasestorage.googleapis.com/v0/b/loomora-cdb63.firebasestorage.app/o/images%2Fproject%2F20.jpeg?alt=media&token=5b2525f4-1815-40f0-a600-8af92e307896",
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/1f4be2de-8156-4d29-df51-82443d8e3e00/kotak300",
     text: "Sentuhan Modern",
     category: "Karpet Kantor",
   },
-
   {
-    src: "https://firebasestorage.googleapis.com/v0/b/loomora-cdb63.firebasestorage.app/o/images%2Fproject%2F2.jpeg?alt=media&token=75eab2cf-d47e-4156-8117-ef0190bbe6de",
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/4876d410-bbb9-4bbe-a543-bd3034b77b00/kotak300",
     text: "Masjid Seskoal - Jakarta",
     category: "Karpet Masjid",
   },
-
   {
-    src: "https://firebasestorage.googleapis.com/v0/b/loomora-cdb63.firebasestorage.app/o/images%2Fproject%2F6.jpeg?alt=media&token=f0aa2e24-dc1f-4a05-98d9-3227f4d84c5f",
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/f11622bf-c08d-456a-789e-12c4af916a00/kotak300",
     text: "Kantor Mabes Polri - Jakarta",
     category: "Karpet Kantor",
   },
-
   {
-    src: "https://firebasestorage.googleapis.com/v0/b/loomora-cdb63.firebasestorage.app/o/images%2Fproject%2F7.jpeg?alt=media&token=4d6947be-b187-4882-b3d9-adeed2973443",
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/a73f434a-e444-4af4-41db-fe9fd3828f00/kotak300",
     text: "Istana Negara Yogyakarta",
     category: "Karpet Ballroom",
   },
-
   {
-    src: "https://firebasestorage.googleapis.com/v0/b/loomora-cdb63.firebasestorage.app/o/images%2Fproject%2F13.jpeg?alt=media&token=c8f12ba2-61d4-41c5-afbc-3604753a8247",
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/78468be4-c4fa-4aef-ed59-b9036cf57a00/kotak300",
     text: "DPRD Pamekasan Jawa Timur",
     category: "Karpet Kantor",
   },
-
   {
-    src: "https://firebasestorage.googleapis.com/v0/b/loomora-cdb63.firebasestorage.app/o/images%2Fproject%2F9.jpeg?alt=media&token=36b8f223-0d19-4e9f-a6bd-810d54b9112f",
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/ce24d606-aa84-45a7-8306-461a79106c00/kotak300",
     text: "Universitas Islam Indonesia",
     category: "Karpet Kantor",
   },
-
   {
-    src: "https://firebasestorage.googleapis.com/v0/b/loomora-cdb63.firebasestorage.app/o/images%2Fproject%2F12.jpeg?alt=media&token=8fb2d24d-c37f-418a-b50c-06f7b2b632b8",
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/5c4d0cf5-f34d-425e-3d86-2144a0c9af00/kotak300",
     text: "Masjid Raya Kota Harapan Indah - Bekasi",
     category: "Karpet Masjid",
+  },
+  {
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/e980db61-8d4d-452f-817e-bb8ce74be700/kotak300",
+    text: "Masjid Agung Manado",
+    category: "Karpet Masjid",
+  },
+  {
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/5f5c1903-37d7-46d4-777f-f2a9bdc58b00/kotak300",
+    text: "Ballroom Hotel Aryaduta Palembang",
+    category: "Karpet Ballroom",
+  },
+  {
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/0e75b034-fbef-4420-6591-4fa41008a900/kotak300",
+    text: "Kantor Dinas Pariwisata Bali",
+    category: "Karpet Kantor",
+  },
+  {
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/1d2dd6cc-8136-4f53-d2db-34bccb694800/kotak300",
+    text: "Masjid Agung Kendari",
+    category: "Karpet Masjid",
+  },
+  {
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/7bf4f0bb-5815-4611-d3e5-e0112e7ef300/kotak300",
+    text: "Gedung Serbaguna Kalimantan Barat",
+    category: "Karpet Ballroom",
+  },
+  {
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/e26d0d5d-2c0b-4226-0044-3e52b1f1e100/kotak300",
+    text: "Kantor Gubernur Sulawesi Utara",
+    category: "Karpet Kantor",
+  },
+  {
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/37e67931-014a-4bbd-c9ea-fa8acf07bf00/kotak300",
+    text: "Masjid Al-Akbar Surabaya",
+    category: "Karpet Masjid",
+  },
+  {
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/eeb4e4cb-94e2-4ba0-b980-68f342816100/kotak300",
+    text: "Gedung Pertemuan Aceh",
+    category: "Karpet Ballroom",
+  },
+  {
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/f09b8dbb-e9e6-4fb0-40cc-e8e33029a300/kotak300",
+    text: "Kantor Pajak Bandung",
+    category: "Karpet Kantor",
+  },
+  {
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/f62ed0b5-4c4d-4432-75f4-72194134a100/kotak300",
+    text: "Masjid Raya Medan",
+    category: "Karpet Masjid",
+  },
+  {
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/99ac339c-81c1-4202-b5f1-b7d9ddc38000/kotak300",
+    text: "Universitas Negeri Makassar",
+    category: "Karpet Kantor",
+  },
+  {
+    src: "https://imagedelivery.net/_tN3dTar-XzU6X9_PBgTbA/bfde99e0-d554-4509-416c-6881e792d600/kotak300",
+    text: "Hotel Horison Jayapura",
+    category: "Karpet Ballroom",
   },
 ];
 
